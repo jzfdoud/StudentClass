0b004e4f5bade8dbd8ff36fb2391daa6a88f20b4
Jada <jada.fagan@gmail.com>
2020-06-18 15:35:31 -0400

    Add project files.

---
+++ StudentClass/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace StudentClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Jada = new Student();
+            Jada.Name = "Jada";
+            Jada.ID = 1;
+            Jada.GPA = 3.5;
+            Jada.SAT = 1600;
+            Jada.TimesTardy = 112;
+            Console.WriteLine($"Jada's name is {Jada.Name}");
+            Console.WriteLine($"Jada's BC score is {Jada.BC(2)}");
+
+
+            var Steve = new Student();
+            Steve.Name = "Steve";
+            Steve.ID = 2;
+            Steve.GPA = 4.0;
+            Steve.SAT = 1599;
+            Steve.TimesTardy = 0;
+            var steveString = Steve.GetValues();
+            Console.WriteLine($"Steve: {steveString}");
+            // calls the BC method for steve's instance
+            Console.WriteLine($"Steve's BC score is {Steve.BC(2)}");
+
+            var Dave = new Student("Dave",100, 1250, 3.5, 2);
+        
+
+        }   
+    }
+
+}
---
+++ StudentClass/Student.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentClass
+{
+    class Student
+    {
+        public double GPA;
+        public int SAT;
+        public int TimesTardy;
+        public string Name;
+        public int ID;
+
+        private double SecretNumberCalculation(double Bnumber, int SecretNumber)
+        {
+            return Bnumber / SecretNumber;
+        }
+
+        public double BC(int SecretNumber)
+        {
+            var dblsat = SAT * 2;
+            var b = dblsat * GPA;
+            var bc = SecretNumberCalculation(b, SecretNumber);
+            return bc;
+        }
+
+        public string GetValues()
+        {
+            //what to do when this is actually called = {}
+            return $"{ID}|{Name}|{SAT}|{GPA}|{TimesTardy}";
+        }
+
+        public Student()
+        {// default constructor, takes no parameters. Contructors exist in oder to initialize
+        }
+        public Student(string Name, int ID, int SAT, double GPA, int TimesTardy)
+        {
+            this.Name = Name;
+            this.ID = ID;
+            this.SAT = SAT;
+            this.GPA = GPA;
+            this.TimesTardy = TimesTardy;
+
+        // <this.> allows the system know that the variable 'name' is a parameter and not a property
+        }
+
+    }
+}
